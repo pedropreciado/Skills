a96f5dbd7c14805791f2e0bd780f8391c4146d74
Pedro Preciado III <pedropreciadoiii@gmail.com>
2017-12-19 03:53:55 -0800

    user controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,13 +1,29 @@
 class Api::UsersController < ApplicationController
   def index
+    @users = User.all - User.find(params[:id])
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      login(@user)
+      render :show
+    else
+      render json: @user.errors.full_messsages, status: 422
+    end
   end
 
   def destroy
+    @user = User.find(params[:id])
+    @user.destroy!
   end
+  
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :photo_url)
+  end
+  
 end
