554e96171260176b5ff937fd030c4511f0cd3a25
Pedro Preciado III <pedropreciadoiii@gmail.com>
2017-12-19 23:20:51 -0800

    add skill controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,32 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'skill/new'
+  end
+
+  namespace :api do
+    get 'skill/index'
+  end
+
+  namespace :api do
+    get 'skill/show'
+  end
+
+  namespace :api do
+    get 'skill/create'
+  end
+
+  namespace :api do
+    get 'skill/edit'
+  end
+
+  namespace :api do
+    get 'skill/update'
+  end
+
+  namespace :api do
+    get 'skill/destroy'
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   namespace :api, defaults: {format: :json} do
