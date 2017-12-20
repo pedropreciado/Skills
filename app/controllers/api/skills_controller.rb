5926cab4abf9e46976ffe5015f266b87fd6d4015
Pedro Preciado III <pedropreciadoiii@gmail.com>
2017-12-19 23:25:35 -0800

    add skill controller =

--- app/controllers/api/skills_controller.rb
+++ app/controllers/api/skills_controller.rb
@@ -3,20 +3,41 @@ class Api::SkillsController < ApplicationController
   end
   
   def index
+    @skills = Skill.all
   end
   
   def show
+    @skill = Skill.find(params[:id])
   end
   
   def create
+    @skill = Skill.new(skill_params)
+    @skill.user_id = current_user.id
+    if @skill.save
+      render :index
+    else
+      render json: @skill.errors.full_messages, status: 422
+    end
   end
   
   def edit 
+    render :edit
   end
   
   def update
   end
   
   def destroy
+    @skill = current_user.skills.find(params[:id])
+    if @skill.destroy!
+      render :show
+    else
+      render json: @skill.errors.full_messages
+    end
   end
+  
+  def skill_params
+    params.require(:skill).permit(:name)
+  end
+  
 end
