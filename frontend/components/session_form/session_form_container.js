c8867950c58aa440716ee172fcae73bef26dc2c9
Pedro Preciado III <pedropreciadoiii@gmail.com>
2017-12-20 16:25:33 -0800

    session form container

---
+++ frontend/components/session_form/session_form_container.js
@@ -0,0 +1,26 @@
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { login, logout, signup, clearSessionErrors } from "../../actions/session_actions";
+import SessionForm from "./session_form";
+
+const mapsStateToProps = (state) => {
+  return {
+    loggedIn: Boolean(state.sessioncurrentUser),
+    errors: state.errors.session
+  }
+};
+
+const mapDispatchToProps = (dispatch, { location }) => {
+  const formType = location.pathname.slice(1);
+  const action = (formType === "login") ? login : signup;
+  return {
+    action: (user) => dispatch(action(user)),
+    clearSessionErrors: () => dispatch(clearSessionErrors()),
+    formType
+  }
+};
+
+export default withRouter(connect)(
+  mapsStateToProps,
+  mapDispatchToProps
+)(SessionForm);
