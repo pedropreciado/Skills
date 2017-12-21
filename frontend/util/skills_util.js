70b16bd5d9bb48f98ac078d591247054eb3c3b45
Pedro Preciado III <pedropreciadoiii@gmail.com>
2017-12-21 14:25:06 -0800

    skills util

---
+++ frontend/util/skills_util.js
@@ -0,0 +1,29 @@
+export const fetchSkills = () => {
+  return $.ajax({
+    method: "GET",
+    url: "/api/skills"
+  })
+}
+
+export const fetchSkill = (id) => {
+  return $.ajax.({
+    method: "GET",
+    url: `/api/skills/${id}`
+  })
+}
+
+export const createSkill = (skill) => {
+  return $.ajax({
+    method: "POST",
+    url: "/api/skills",
+    data: { skill }
+  })
+}
+
+export const endorseSkill = (id) => {
+  return $.ajax({
+    method: "POST",
+    url: "/api/endorsements",
+    data: { id }
+  })
+}
