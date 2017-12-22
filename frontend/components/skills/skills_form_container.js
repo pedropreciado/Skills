e789848c0ab8ff888185f120a2a28c179f5f17c1
Pedro Preciado III <pedropreciadoiii@gmail.com>
2017-12-21 21:46:29 -0800

    skill form container

---
+++ frontend/components/skills/skills_form_container.js
@@ -0,0 +1,24 @@
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import SkillsForm from "./skills_form";
+
+import { fetchSkill, createSkill } from "../../actions/skills_actions";
+
+const mapStatetoProps = (state, ownProps) => {
+  let currentUser = state.session.currentUser;
+  return {
+    currentUser
+  }
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    fetchSkill: (id) => dispatch(fetchSkill(id)),
+    createSkill: (skill) => dispatch(createSkill(skill))
+  }
+}
+
+export default withRouter(connect(
+  mapStatetoProps,
+  mapDispatchToProps
+)(SkillsForm))
