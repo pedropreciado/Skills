6e0a09a1ae1929c8ef69f16c162b322c7bf167b0
Pedro Preciado III <pedropreciadoiii@gmail.com>
2017-12-21 16:00:02 -0800

    skills acctions

---
+++ frontend/actions/skills_actions.js
@@ -0,0 +1,31 @@
+import * as SkillsApiUtil from "../util/skills_util";
+
+export const RECEIVE_ALL_SKILLS = "RECEIVE_ALL_SKILLS";
+export const RECEIVE_SKILL = "RECEIVE_SKILL";
+export const RECEIVE_ENDORSEMENT = "RECEIVE_ENDORSEMENT";
+
+export const fetchSkills = () => dispatch => (
+  SkillsApiUtil.fetchSkills().then((skills) => dispatch(receiveAllSkills(skills)))
+);
+
+export const fetchSkill = skill => dispatch => (
+  SkillsApiUtil.fetchSkill(skill).then((skill) => dispatch(receiveSkill(skill)))
+);
+
+export const createSkill = (skill) => dispatch => (
+  SkillsApiUtil.createSkill(skill).then((skill) => dispatch(receiveSkill(skill)))
+);
+
+export const endorseSkill = (id) => dispatch => (
+  SkillsApiUtil.endorseSkill(id).then((skill) dispatch(receiveSkill(skill)))
+)
+
+const receiveAllSkills = skills => ({
+  type: RECEIVE_ALL_SKILLS,
+  skills
+});
+
+const receiveSkill = skill => ({
+  type: RECEIVE_SKILL,
+  skill
+});
