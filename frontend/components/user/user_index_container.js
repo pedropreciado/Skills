d6919fa8e2419a2ffce1097474e6874f8bf604c8
Pedro Preciado III <pedropreciadoiii@gmail.com>
2017-12-22 01:17:29 -0800

    user cidx container

---
+++ frontend/components/user/user_index_container.js
@@ -0,0 +1,19 @@
+import { connect } from "react-redux";
+import { fetchUser, fetchUsers } from "../../actions/users_actions";
+import UserIndex from "./user_index";
+const mapStateToProps = (state) => {
+  return {
+    users: Object.keys(state.users).map((id) => state.users[id]),
+    currentUser: state.session.currentUser
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchUsers: () => dispatch(fetchUsers()),
+  fetchUser: (id) => dispatch(fetchUser(id))
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(UserIndex)
