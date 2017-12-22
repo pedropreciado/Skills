39b0248dd6cae775a421bdc3bb519b325f3daa2d
Pedro Preciado III <pedropreciadoiii@gmail.com>
2017-12-21 16:04:59 -0800

    update root reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import SessionReducer from "./session_reducer";
 import ErrorsReducer from "./errors_reducer";
+import SkillsReducer from "./skills_reducer";
 
 export default combineReducers({
   session: SessionReducer,
-  errors: ErrorsReducer
+  errors: ErrorsReducer,
+  skills: SkillsReducer
 })
