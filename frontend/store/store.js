21f824a5cdd346f037fde5e721cde23762186a8a
Pedro Preciado III <pedropreciadoiii@gmail.com>
2017-12-28 17:42:18 -0800

    remove redux-logger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import RootReducer from "../reducers/root_reducer";
-import logger from "redux-logger";
+// import logger from "redux-logger";
 
 // const middlewares = [thunk, logger];
 const middlewares = [thunk];
